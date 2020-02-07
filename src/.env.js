c8982b9a08a3cc43818308bebbb59df2463db3e9
steven victor <chikodi543@gmail.com>
2020-02-07 14:55:32 +0100

    Add production url

--- src/.env.js
+++ src/.env.js
@@ -2,6 +2,6 @@ let API_ROUTE
 
 process.env.NODE_ENV == 'development'
   ? API_ROUTE = 'http://127.0.0.1:8888'
-  : API_ROUTE = 'https://sam.com'
+  : API_ROUTE = 'https://food-app-golang.herokuapp.com'
 
 export default API_ROUTE
